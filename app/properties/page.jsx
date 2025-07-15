3d923aa84f8cfb23bc3c341b59bcce47c24db701
Kushal Borra <kushalborra97@gmail.com>
2025-07-15 11:17:34 +0530

    Add property cards

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,25 @@
-const ProperiesPage = () => {
-    return ( <div>Properties Page</div> );
-}
- 
+import PropertyCard from "@/components/PropertyCard";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+const ProperiesPage = async () => {
+  await connectDB();
+  const properties = await Property.find({}).lean(); // lean() fetches as json, use for GET
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No Properties Found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
 export default ProperiesPage;
